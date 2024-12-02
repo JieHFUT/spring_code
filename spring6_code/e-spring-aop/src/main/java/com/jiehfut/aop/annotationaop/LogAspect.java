5ca5f5a7eb93e27497df87ea0023a56e93342215
JieCHERY <9594523wrj@gmail.com>
2024-12-02 16:04:18 +0800

    aop

--- spring6_code/e-spring-aop/src/main/java/com/jiehfut/aop/annotationaop/LogAspect.java
+++ spring6_code/e-spring-aop/src/main/java/com/jiehfut/aop/annotationaop/LogAspect.java
@@ -1,8 +1,12 @@
 package com.jiehfut.aop.annotationaop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.junit.jupiter.api.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 // 切面类
 @Aspect // 表示是一个切面类
@@ -17,16 +21,84 @@ public class LogAspect {
     // 异常 @AfterThrowing、
     // 后置 @After()、
     // 环绕 @Around()、
-    @Before(value = "execution(public int com.jiehfut.aop.annotationaop.CalculatorImpl.add(int, int ))")
-    public void beforeMethod() {
 
-        System.out.println("Logger-->前置通知");
+
+
+
+
+
+
+    // 1.前置 @Before(value = "切入点表达式配置切入点")、
+    @Before(value = "execution(public int com.jiehfut.aop.annotationaop.CalculatorImpl.add(int, int ))")
+    public void beforeMethod(JoinPoint joinPoint) {
+        // 使用 joinPoint 可以获得增强方法的相关信息
+        String name = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        System.out.println("Logger-->前置通知，方法名称是：" + name + "，参数是：" + Arrays.toString(args));
     }
 
 
+    // 2.后置 @After()、
+    // @After(value = "pointCut()")
+    // @After(value = "com.jiehfut.aop.annotationaop.LogAspect.pointCut()")
+    // @After(value = "execution(* com.jiehfut.aop.annotationaop.CalculatorImpl.*(..))")
+    //
+    @After(value = "pointCut()")
+    public void afterMethod(JoinPoint joinPoint) {
+        // 使用 joinPoint 可以获得增强方法的相关信息
+        String name = joinPoint.getSignature().getName();
+        System.out.println("Logger-->后置通知，方法名称是：" + name);
+    }
 
 
-    // 2.
+    // 3.返回 @AfterReturning、增强方法可以得到目标方法的返回值：returning = "result"
+    @AfterReturning(value = "execution(* com.jiehfut.aop.annotationaop.CalculatorImpl.*(..))", returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        // 使用 joinPoint 可以获得增强方法的相关信息
+        String name = joinPoint.getSignature().getName();
+        System.out.println("Logger-->返回通知，方法名称是：" + name + "，目标方法的返回结果是：" + result);
+    }
 
-    // 3.
+
+    // 4.异常 @AfterThrowing、如果你的目标方法执行过程中出现了异常就会执行，否则不会执行
+    @AfterThrowing(value = "execution(* com.jiehfut.aop.annotationaop.CalculatorImpl.*(..))", throwing = "ex")
+    public void afterThrowing(JoinPoint joinPoint, Exception ex) {
+        // 使用 joinPoint 可以获得增强方法的相关信息
+        String name = joinPoint.getSignature().getName();
+        System.out.println("Logger-->异常通知，方法名称是：" + name + "，异常信息是：" + ex);
+    }
+
+
+    // 5.环绕 @Around()、针对 try-catch
+    @Around(value = "execution(* com.jiehfut.aop.annotationaop.CalculatorImpl.*(..))")
+    public Object afterAround(ProceedingJoinPoint joinPoint) {
+        // 使用 joinPoint 可以获得增强方法的相关信息
+        String name = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        String argsStr = Arrays.toString(args);
+        Object result = null;
+        try{
+            System.out.println("环绕通知 => 目标方法之前执行");
+            // 通过 ProceedingJoinPoint 调用目标方法
+            result = joinPoint.proceed();
+
+            System.out.println("环绕通知 => 目标方法返回值之后执行");
+        } catch (Throwable throwable) {
+            System.out.println("环绕通知 => 目标方法出现异常执行");
+        } finally {
+            System.out.println("环绕通知 => 目标方法执行完毕后执行");
+        }
+        return result;
+    }
+
+
+    // 重用切入点表达式
+    @Pointcut(value = "execution(* com.jiehfut.aop.annotationaop.CalculatorImpl.*(..))")
+    public void pointCut() {}
+
+    // 切面的优先级 @Order() 来控制
+
+
+
+
 }
--- spring6_code/e-spring-aop/src/main/java/com/jiehfut/aop/annotationaop/TestCalcuator.java
+++ spring6_code/e-spring-aop/src/main/java/com/jiehfut/aop/annotationaop/TestCalcuator.java
@@ -11,9 +11,15 @@ public class TestCalcuator {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
         Calculator calculator = context.getBean(Calculator.class);
         calculator.add(12, 23);
+
         /**
-         * Logger-->前置通知
+         * 环绕通知 => 目标方法之前执行
+         * Logger-->前置通知，方法名称是：add，参数是：[12, 23]
          * 方法内部 result = 35
+         * Logger-->返回通知，方法名称是：add，目标方法的返回结果是：35
+         * Logger-->后置通知，方法名称是：add
+         * 环绕通知 => 目标方法返回值之后执行
+         * 环绕通知 => 目标方法执行完毕后执行
          */
     }
 
