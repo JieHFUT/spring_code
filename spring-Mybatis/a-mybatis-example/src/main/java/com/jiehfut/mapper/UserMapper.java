92606f5dad83ef423973cc1197a37185373884a6
JieHFUT <9594523wrj@gmail.com>
2024-12-11 01:34:58 +0800

    interceptor

--- spring-Mybatis/a-mybatis-example/src/main/java/com/jiehfut/mapper/UserMapper.java
+++ spring-Mybatis/a-mybatis-example/src/main/java/com/jiehfut/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.jiehfut.mapper;
 
 import com.jiehfut.pojo.User;
 
+import java.util.List;
+
 /**
  * UserMapper
  * mybatis 面向接口编程
@@ -13,5 +15,23 @@ public interface UserMapper {
      */
     int insertUser();
 
+    /**
+     * 修改用户数据信息
+     */
+    void updateUser();
+
+    /**
+     * 删除用户信息
+     */
+    void deleteUser();
+
+    /**
+     * 查询结果为一条实体类对象
+     */
+    User selectUserById(int id);
+    /**
+     * 查询所有数据集合
+     */
+    List<User> selectAllUser();
 
 }
--- spring-Mybatis/a-mybatis-example/src/test/java/com/jiehfut/test/MybatisTest.java
+++ spring-Mybatis/a-mybatis-example/src/test/java/com/jiehfut/test/MybatisTest.java
@@ -1,6 +1,7 @@
 package com.jiehfut.test;
 
 import com.jiehfut.mapper.UserMapper;
+import com.jiehfut.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 
 public class MybatisTest {
@@ -28,11 +30,128 @@ public class MybatisTest {
         // 5.我们传进去一个类的类型的时候，就返回一个该类的对象，可以帮助我们返回一个接口的实现类的对象，使用的是代理模式
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
 
-
+        // 测试功能
         int result = userMapper.insertUser();
         System.out.println("result = " + result);
 
         // 提交事务
         sqlSession.commit();
     }
+
+
+    /**
+     * SqlSession 默认不会自动提交事务，可以通过设置让其自动提交事务
+     * @throws IOException
+     */
+    @Test
+    public void testMybatisCommit() throws IOException {
+        // mybatis 为我们提供了一个 sqlsession 对象用于操作
+
+        // 1.加载核心配置文件（以字节输入流获取）
+        InputStream stream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 2.获取 SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // 3.获取 SqlSessionFactory 对象
+        SqlSessionFactory factory = builder.build(stream);
+        // 4.获取 sqlsession 对象（代表 Java 程序和数据库之间的会话）
+        // mybatis 不会像 jdbc 一样自动提交事务，可以通过设置 true 让其自动提交事务
+        SqlSession sqlSession = factory.openSession(true);
+
+        // 5.我们传进去一个类的类型的时候，就返回一个该类的对象，可以帮助我们返回一个接口的实现类的对象，使用的是代理模式
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        // 测试功能
+        int result = userMapper.insertUser();
+        System.out.println("result = " + result);
+    }
+
+
+    @Test
+    public void testUpdate() throws IOException {
+        // mybatis 为我们提供了一个 sqlsession 对象用于操作
+
+        // 1.加载核心配置文件（以字节输入流获取）
+        InputStream stream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 2.获取 SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // 3.获取 SqlSessionFactory 对象
+        SqlSessionFactory factory = builder.build(stream);
+        // 4.获取 sqlsession 对象（代表 Java 程序和数据库之间的会话）
+        // mybatis 不会像 jdbc 一样自动提交事务，可以通过设置 true 让其自动提交事务
+        SqlSession sqlSession = factory.openSession(true);
+
+        // 5.我们传进去一个类的类型的时候，就返回一个该类的对象，可以帮助我们返回一个接口的实现类的对象，使用的是代理模式
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        userMapper.updateUser();
+
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        // mybatis 为我们提供了一个 sqlsession 对象用于操作
+
+        // 1.加载核心配置文件（以字节输入流获取）
+        InputStream stream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 2.获取 SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // 3.获取 SqlSessionFactory 对象
+        SqlSessionFactory factory = builder.build(stream);
+        // 4.获取 sqlsession 对象（代表 Java 程序和数据库之间的会话）
+        // mybatis 不会像 jdbc 一样自动提交事务，可以通过设置 true 让其自动提交事务
+        SqlSession sqlSession = factory.openSession(true);
+
+        // 5.我们传进去一个类的类型的时候，就返回一个该类的对象，可以帮助我们返回一个接口的实现类的对象，使用的是代理模式
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        userMapper.deleteUser();
+    }
+
+    @Test
+    public void testSelectById() throws IOException {
+        // mybatis 为我们提供了一个 sqlsession 对象用于操作
+
+        // 1.加载核心配置文件（以字节输入流获取）
+        InputStream stream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 2.获取 SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // 3.获取 SqlSessionFactory 对象
+        SqlSessionFactory factory = builder.build(stream);
+        // 4.获取 sqlsession 对象（代表 Java 程序和数据库之间的会话）
+        // mybatis 不会像 jdbc 一样自动提交事务，可以通过设置 true 让其自动提交事务
+        SqlSession sqlSession = factory.openSession(true);
+
+        // 5.我们传进去一个类的类型的时候，就返回一个该类的对象，可以帮助我们返回一个接口的实现类的对象，使用的是代理模式
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User user = userMapper.selectUserById(2);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testSelectAllUser() throws IOException {
+        // mybatis 为我们提供了一个 sqlsession 对象用于操作
+
+        // 1.加载核心配置文件（以字节输入流获取）
+        InputStream stream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 2.获取 SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // 3.获取 SqlSessionFactory 对象
+        SqlSessionFactory factory = builder.build(stream);
+        // 4.获取 sqlsession 对象（代表 Java 程序和数据库之间的会话）
+        // mybatis 不会像 jdbc 一样自动提交事务，可以通过设置 true 让其自动提交事务
+        SqlSession sqlSession = factory.openSession(true);
+
+        // 5.我们传进去一个类的类型的时候，就返回一个该类的对象，可以帮助我们返回一个接口的实现类的对象，使用的是代理模式
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = userMapper.selectAllUser();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+    }
+
+
+
+
+
+
+
 }
