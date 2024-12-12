3adb0a0ef7f7987e03429448d13aa312ed71759b
JieCHERY <9594523wrj@gmail.com>
2024-12-12 18:04:23 +0800

    cache

--- spring-Mybatis/d-mybatis-dynamicSQL/src/main/java/com/jiehfut/mapper/CacheMapper.java
+++ spring-Mybatis/d-mybatis-dynamicSQL/src/main/java/com/jiehfut/mapper/CacheMapper.java
@@ -5,12 +5,15 @@ import org.apache.ibatis.annotations.Param;
 
 public interface CacheMapper {
     /**
-     * 测试缓存
+     * 测试一级缓存
      */
     Emp getEmpById(@Param("eid")Integer eid);
     /**
      * 添加操作
      */
     void insertEmp(Emp emp);
+
+
+
 
 }
--- spring-Mybatis/d-mybatis-dynamicSQL/src/test/java/conm/jiehfut/CacheMapperTest.java
+++ spring-Mybatis/d-mybatis-dynamicSQL/src/test/java/conm/jiehfut/CacheMapperTest.java
@@ -3,8 +3,15 @@ package conm.jiehfut;
 import com.jiehfut.mapper.CacheMapper;
 import com.jiehfut.pojo.Emp;
 import com.jiehfut.utils.SqlSessionUtil;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class CacheMapperTest {
 
@@ -98,6 +105,53 @@ public class CacheMapperTest {
          * 可见任意的一次增删改之后就会清空一级缓存
          */
     }
+
+
+    /**
+     * 下面是对二级缓存的测试
+     *
+     * a>在核心配置文件中，设置全局配置属性cacheEnabled="true"，默认为 true，不需要设置
+     * b>在映射文件中设置标签 <cache/>
+     * c>二级缓存必须在 SqlSession 关闭或提交之后有效
+     * d>查询的数据所转换的实体类类型必须实现序列化的接口
+     *
+     *
+     * 两次查询之间执行了任意的增删改，会使一级和二级缓存同时失效
+     */
+    @Test
+    public void testTwoCache(){
+        try {
+            InputStream stream = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(stream);
+            SqlSession session1 = factory.openSession(true);
+            SqlSession session2 = factory.openSession(true);
+
+
+            CacheMapper mapper1 = session1.getMapper(CacheMapper.class);
+            CacheMapper mapper2 = session2.getMapper(CacheMapper.class);
+            System.out.println(mapper1.getEmpById(2));
+            session1.close();
+            System.out.println(mapper2.getEmpById(2));
+            session2.close();
+
+            /**
+             * DEBUG 12-12 18:00:50,241 Cache Hit Ratio [com.jiehfut.mapper.CacheMapper]: 0.0  (LoggingCache.java:60)
+             * DEBUG 12-12 18:00:50,722 ==>  Preparing: select * from t_emp where eid = ?  (BaseJdbcLogger.java:137)
+             * DEBUG 12-12 18:00:50,768 ==> Parameters: 2(Integer)  (BaseJdbcLogger.java:137)
+             * DEBUG 12-12 18:00:50,814 <==      Total: 1  (BaseJdbcLogger.java:137)
+             * Emp(eid=2, empName=null, age=32, sex=男, email=42529@163.com, dept=null)
+             * WARN  12-12 18:00:50,851 As you are using functionality that deserializes object streams, it is recommended to define the JEP-290 serial filter. Please refer to https://docs.oracle.com/pls/topic/lookup?ctx=javase15&id=GUID-8296D8E8-2B93-4B9A-856E-0A65AF9B8C66  (SerialFilterChecker.java:46)
+             * DEBUG 12-12 18:00:50,856 Cache Hit Ratio [com.jiehfut.mapper.CacheMapper]: 0.5  (LoggingCache.java:60)
+             * Emp(eid=2, empName=null, age=32, sex=男, email=42529@163.com, dept=null)
+             *
+             * 可见在第一次查询后关闭 sqlsession 后，再次查询发现数据已经被放在二级缓存中
+             */
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
 
 
