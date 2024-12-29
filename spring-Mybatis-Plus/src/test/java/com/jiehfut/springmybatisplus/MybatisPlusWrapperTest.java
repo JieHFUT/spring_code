4f874124973a20bbd99a9a55cddde4b0614668e1
JieCHERY <9594523wrj@gmail.com>
2024-12-29 18:45:15 +0800

    mybatis-plus

--- spring-Mybatis-Plus/src/test/java/com/jiehfut/springmybatisplus/MybatisPlusWrapperTest.java
+++ spring-Mybatis-Plus/src/test/java/com/jiehfut/springmybatisplus/MybatisPlusWrapperTest.java
@@ -1,10 +1,14 @@
 package com.jiehfut.springmybatisplus;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.jiehfut.springmybatisplus.mapper.UserMapper;
 import com.jiehfut.springmybatisplus.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -110,6 +114,101 @@ public class MybatisPlusWrapperTest {
     }
 
 
+    @Test // 测试子查询的功能
+    public void testQuery(){
+        // 查询 id 小于等于 100 的客户信息
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.inSql("uid", "select uid from t_user where uid <= 100");
+        List<User> users = userMapper.selectList(userQueryWrapper);
+        users.forEach(System.out::println);
+        // SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user
+        // WHERE is_deleted=0 AND (uid IN (select uid from t_user where uid <= 100))
+    }
+
+
+
+//  <================================================================>
+    @Test // 测试使用 UpdateWrapper 修改信息
+    public void testUpdateWrapper(){
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        // 将用户名中包含有a并且（年龄大于20或邮箱为null）的用户信息修改
+        updateWrapper.like("user_name", "a")
+                .and(i -> i.gt("age", 20).or().isNull("email"));
+        updateWrapper.set("user_name", "dahuang")
+                .set("email", "dahuang@163.com");
+        int update = userMapper.update(null, updateWrapper);
+        System.out.println("update = " + update);
+        // UPDATE t_user SET user_name=?,email=? WHERE is_deleted=0
+        // AND (user_name LIKE ? AND (age > ? OR email IS NULL))
+    }
+
+
+
+    @Test // 判断 sql，相当于动态 sql
+    public void testDynamic(){
+        String username = "";
+        Integer ageBegin = 20;
+        Integer ageEnd = 40;
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(username)) {
+            // 判断某一个字符串是否不为空字符串并且不为 null，且不为空白符
+            userQueryWrapper.like("user_name", username);
+        }
+        if (ageBegin != null) {
+            userQueryWrapper.ge("age", ageBegin);
+        }
+        if (ageBegin != null) {
+            userQueryWrapper.le("age", ageEnd);
+        }
+        List<User> users = userMapper.selectList(userQueryWrapper);
+        users.forEach(System.out::println);
+        // SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user
+        // WHERE is_deleted=0 AND (age >= ? AND age <= ?)
+    }
+
+    @Test // 简化的动态 sql
+    public void testSimpleDynamic(){
+        String username = "";
+        Integer ageBegin = 20;
+        Integer ageEnd = 40;
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.like(StringUtils.isNotBlank(username), "user_name", username)
+                .ge(ageBegin != null,"age", ageBegin)
+                .le(ageEnd != null,"age", ageEnd);
+        List<User> users = userMapper.selectList(userQueryWrapper);
+        users.forEach(System.out::println);
+        // SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user
+        // WHERE is_deleted=0 AND (age >= ? AND age <= ?)
+    }
+
+
+    @Test // 使用 lambda 表达式 => User::getAge 获取字段名
+    public void testLambdaQuery(){
+        String username = "a";
+        Integer ageBegin = null;
+        Integer ageEnd = 30;
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.isNotBlank(username), User::getName, username)
+                .ge(ageBegin != null, User::getAge, ageBegin)
+                .le(ageEnd != null, User::getAge, ageEnd);
+        userMapper.selectList(queryWrapper).forEach(System.out::println);
+        // SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user
+        // WHERE is_deleted=0 AND (user_name LIKE ? AND age <= ?)
+    }
+
+    @Test // 测试使用 UpdateWrapper 修改信息
+    public void testLambdaUpdateWrapper(){
+        LambdaUpdateWrapper<User> updateWrapper = new LambdaUpdateWrapper<>();
+        // 将用户名中包含有a并且（年龄大于20或邮箱为null）的用户信息修改
+        updateWrapper.like(User::getName, "a")
+                .and(i -> i.gt(User::getAge, 20).or().isNull(User::getEmail));
+        updateWrapper.set(User::getName, "dahuang")
+                .set(User::getEmail, "dahuang@163.com");
+        int update = userMapper.update(updateWrapper);
+        System.out.println("update = " + update);
+        // UPDATE t_user SET user_name=?,email=? WHERE is_deleted=0
+        // AND (user_name LIKE ? AND (age > ? OR email IS NULL))
+    }
 
 
 
