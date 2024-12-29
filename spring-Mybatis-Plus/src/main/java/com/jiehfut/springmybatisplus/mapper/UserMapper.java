48ca4f5db3eeabe730373a9457169addfd04985a
JieCHERY <9594523wrj@gmail.com>
2024-12-29 19:15:55 +0800

    mybatis-plus

--- spring-Mybatis-Plus/src/main/java/com/jiehfut/springmybatisplus/mapper/UserMapper.java
+++ spring-Mybatis-Plus/src/main/java/com/jiehfut/springmybatisplus/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.jiehfut.springmybatisplus.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jiehfut.springmybatisplus.pojo.User;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
@@ -19,5 +21,15 @@ public interface UserMapper extends BaseMapper<User> {
      * @param id
      * @return
      */
-    Map<String, Object> selectMapById(Long id);
+    Map<String, Object> selectMapById(@Param("id") Long id);
+
+
+    /**
+     * 自定义根据年龄来查询记录，然后分页
+     * @param page mybatis-plus 所提供的分页对象，必需位于第一个参数
+     * @param age
+     * @return
+     */
+    Page<User> selectPageVo(@Param("page") Page<User> page, @Param("age") Integer age);
+
 }
--- spring-Mybatis-Plus/src/test/java/com/jiehfut/springmybatisplus/MyBatisPlusPluginsPageTest.java
+++ spring-Mybatis-Plus/src/test/java/com/jiehfut/springmybatisplus/MyBatisPlusPluginsPageTest.java
@@ -14,15 +14,41 @@ public class MyBatisPlusPluginsPageTest {
     @Autowired
     private UserMapper userMapper;
 
-    @Test
+    @Test // 测试分页插件的使用
     public void testPagePlugins(){
-        // 测试分页插件的使用
         Page<User> page = new Page<>(2, 3); // 当前页码，每一页条数
         userMapper.selectPage(page, null); // 查询所有记录
         System.out.println(page);
         // SELECT uid AS id,user_name AS name,age,email,is_deleted FROM t_user
         // WHERE is_deleted=0 LIMIT ?,?
+
+        System.out.println("page.getRecords = " + page.getRecords());
+        System.out.println("page.getTotal = " + page.getTotal());
+        System.out.println("page.getPages = " + page.getPages());
+        System.out.println("page.hasNext = " + page.hasNext());
+        System.out.println("page.hasPrevious = " + page.hasPrevious());
+        // page.getRecords = [User(id=4, name=zhangsan, age=18, email=346122@163.com, isDeleted=0),
+        //                    User(id=5, name=lisi, age=23, email=43652342@163.com, isDeleted=0),
+        //                    User(id=6, name=dahuang, age=63, email=dahuang@163.com, isDeleted=0)]
+        // page.getTotal = 7
+        // page.getPages = 3
+        // page.hasNext = true
+        // age.hasPrevious = true
     }
 
-    
+
+    @Test
+    public void testSelfPageByAge(){
+        Page<User> page = new Page<>(2, 3);
+        userMapper.selectPageVo(page, 20);
+        System.out.println(page.getRecords());
+        // select uid, user_name, age, email from t_user where age > ? LIMIT ?,?
+    }
+
+
+
+
+
+
+
 }
